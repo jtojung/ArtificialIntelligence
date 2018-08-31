0cf8fff13def37e0bbe3693e8d55770a1fc43c70
threepat.kiatkamol <threepat.k@gmail.com>
2018-08-31 13:33:40 +1000

    add more

---
+++ Assets/SteeringBehaviours/Script/AIAgentDirector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIAgentDirector : MonoBehaviour {
+
+    public AIAgent agent;
+
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(camRay, out hit, 1000f))
+        {
+            agent.SetTarget(hit.point);
+        }
+	}
+}
---
+++ Assets/SteeringBehaviours/Script/CameraBounds.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBounds : MonoBehaviour
+{
+    public Vector3 size = new Vector3(50f, 0f, 20f);
+
+    public Vector3 GetAdjustedPositon(Vector3 incomingPos)
+    {
+        // Get bounds position
+        Vector3 pos = transform.position;
+        Vector3 halfSize = size * 0.5f;
+
+        if(incomingPos.z > pos.z + halfSize.z)
+        {
+            incomingPos.z = pos.z + halfSize.z;
+        }
+
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
